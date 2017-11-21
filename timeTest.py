70d06cc7d4481005e04cf47431888d852cc6aa85
Peter Gillis <petergillis@Peters-MacBook-Air.local>
2017-11-21 17:40:19 -0400

    added user input customization

--- timeTest.py
+++ timeTest.py
@@ -1,14 +1,22 @@
 if __name__ == '__main__':
     import time
+    while True:
+        print("How many iterations of each function would you like to run?")
+        try:
+            iterations = int(input())
+            break
+        except:
+            print("Please input an integer value.")
     normal_time = 0
     list_comp_time = 0
     def runFuncs():
         global normal_time
         global list_comp_time
         def fizzNormal():
+            global iterations
             global normal_time
             start = time.time()
-            for num in range(1,100):
+            for num in range(1,iterations):
                 if num%3==0 and num%5==0:
                     print("FizzBuzz")
                 elif num%3==0:
@@ -20,9 +28,10 @@ if __name__ == '__main__':
             end = time.time()
             normal_time = (end - start)
         def fizzListComp():
+            global iterations
             global list_comp_time
             start = time.time()
-            print(*['fizzbuzz' if num%3==0 and num%5==0 else 'fizz' if num%3==0 else 'buzz' if num%5==0 else num for num in range(1,100)], sep='\n')
+            print(*['fizzbuzz' if num%3==0 and num%5==0 else 'fizz' if num%3==0 else 'buzz' if num%5==0 else num for num in range(1,iterations)], sep='\n')
             end = time.time()
             list_comp_time = (end - start)
         fizzNormal()
