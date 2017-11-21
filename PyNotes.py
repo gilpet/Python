0765e7aa835fffcf01a5ac36bd5a94e88c4295cd
gilpet <petergillis11@gmail.com>
2017-11-21 13:31:01 -0400

    comp

--- PyNotes.py
+++ PyNotes.py
@@ -323,18 +323,59 @@ for word in s.split():
 print([word for word in st.split() if len(word)%2==0])
 #program that prints the integers 1 to 100 but multiples of 3 print fizz, and
 #multiples of 5 print buzz. fizzbuzz for both
-for num in xrange(1,100):
-    if num%3==0 and num%5==0:
-        print "FizzBuzz"
-    elif num%3==0:
-        print "Fizz"
-    elif num%5==0:
-        print "Buzz"
-    else:
-        print num
+import time
+normal_time = 0
+list_comp_time = 0
+def runFuncs():
+    global normal_time
+    global list_comp_time
+    def fizzNormal():
+        global normal_time
+        start = time.time()
+        for num in range(1,10000):
+            if num%3==0 and num%5==0:
+                print("FizzBuzz")
+            elif num%3==0:
+                print("Fizz")
+            elif num%5==0:
+                print("Buzz")
+            else:
+                print(num)
+        end = time.time()
+        normal_time = (end - start)
+    def fizzListComp():
+        global list_comp_time
+        start = time.time()
+        print(*['fizzbuzz' if num%3==0 and num%5==0 else 'fizz' if num%3==0 else 'buzz' if num%5==0 else num for num in range(1,10000)], sep='\n')
+        end = time.time()
+        list_comp_time = (end - start)
+    fizzNormal()
+    fizzListComp()
+    print('Normal for loop: %.10f'%(normal_time))
+    print('Fancy list comprehension: %.10f'%(list_comp_time))
+
+
+def fizzNormal():
+    start = time.time()
+    for num in range(1,10000):
+        if num%3==0 and num%5==0:
+            print("FizzBuzz")
+        elif num%3==0:
+            print("Fizz")
+        elif num%5==0:
+            print("Buzz")
+        else:
+            print(num)
+    end = time.time()
+    normal_time = (end - start)
 
 #solved using list comprehension
-print(*['fizzbuzz' if num%3==0 and num%5==0 else 'fizz' if num%3==0 else 'buzz' if num%5==0 else num for num in range(1,100)], sep='\n')
+def fizzListComp():
+    start = time.time()
+    print(*['fizzbuzz' if num%3==0 and num%5==0 else 'fizz' if num%3==0 else 'buzz' if num%5==0 else num for num in range(1,10000)], sep='\n')
+    end = time.time()
+    list_comp_time = (end - start)
+
 st = "create a list of the first letters of every word in this string"
 l1 = [word[0] for word in st.split()]
 
